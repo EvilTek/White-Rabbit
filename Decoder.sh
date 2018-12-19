bb824dbfba18be14f7fd66b8d799412fd5a3aab0
KoNxT3D <28337515+KoNxT3D@users.noreply.github.com>
2018-12-18 23:05:57 -0800

    Update Decoder.sh

--- Decoder.sh
+++ Decoder.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# Tis is the easy part of the project. All we have to do now is unzip all files in Vault..
+# This is the easy part of the project. All we have to do now is unzip all files in Vault..
 cd Vault
 unzip \*.png
